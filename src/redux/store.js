5540338e041d34fe5774aa21204facb6f7b54fa1
xXangelXx <angeluchimag17@gmail.com>
2021-03-04 12:40:22 -0400

    redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';/*
+esto funciona para monitorear los estados de redux en consola
+del navegador*/
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import getApiDog from './reducers/apiDogs';
 const reducer = combineReducers({
     getApiDog
 })
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    composeWithDevTools( applyMiddleware(logger ,thunk) )
+);
